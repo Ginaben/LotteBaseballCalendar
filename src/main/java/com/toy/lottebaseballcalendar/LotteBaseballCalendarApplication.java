ea01b7dbd7c82a02c8e753664d8e2ae799abf516
Ginaben <iamgood147@gmail.com>
2024-04-01 20:26:52 +0900

    Initial commit

---
+++ src/main/java/com/toy/lottebaseballcalendar/LotteBaseballCalendarApplication.java
@@ -0,0 +1,13 @@
+package com.toy.lottebaseballcalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteBaseballCalendarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteBaseballCalendarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toy/lottebaseballcalendar/LotteBaseballCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.lottebaseballcalendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteBaseballCalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
