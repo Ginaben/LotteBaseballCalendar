0af94f3b13f606293b336daae3f3ac54f856d53a
Ginaben <iamgood147@gmail.com>
2024-04-07 00:42:48 +0900

    현재 날짜 추가

--- src/main/java/com/toy/lottebaseballcalendar/Main/MainController.java
+++ src/main/java/com/toy/lottebaseballcalendar/Main/MainController.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 @Controller
 @NoArgsConstructor
@@ -18,6 +22,11 @@ public class MainController {
 
     @GetMapping("/")
     public String getMatchList(Model model) throws Exception {
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일");
+        String today = currentDate.format(formatter);
+        model.addAttribute("today", today);
 
         List matchList = mainService.teamMatch();
         model.addAttribute("matchList", matchList);
